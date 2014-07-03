2ef70f5c6a4a114510b4a0ee7972f1ea9bd881eb
Will Bamford <w.bamford@gmail.com>
2014-07-03 08:20:51 +0100

    Sketch out how the renderer could work with a bit of pseudo-code.

--- polygonjs/Renderer.js
+++ polygonjs/Renderer.js
@@ -13,6 +13,46 @@ define([], function () {
     Renderer.prototype = {
         draw: function (delta) {
             
+            var polygons = this.scene.polygons;
+            var polygon;
+            var i = polygons.length;
+            while (--i >= 0) {
+                polygon = polygons[i];
+                // polygon.updateViewVertices(camera.projection);
+            }
+
+            /*
+                TODO:
+                =====
+
+                // Assuming polygon / camera / light world vertices have been
+                // calculated properly.
+
+                var camera = scene.mainCamera;
+                scene.polygons.each(function (polygon) {
+                    // camera.updateViewVertices(polygon);
+                    polygon.updateScreenVertices(camera.projection); // M.
+
+                    if (polygon.isVisible)
+                        polygon.updateLighting(scene.lights);
+
+                    // polygon.isVisible (based on backface cull and facing of normal)
+                    // Note: there may be a "z" component to screen vertex
+                    // which can be used for sorting.
+                    // polygon.worldVertices.each(function (wv, index) {
+                    //    polygon.screenVertices[index] = camera.project(wv);
+                    //});
+                });
+
+                sortPolygonsByDepth(scene.polygons); // M.
+
+                scene.polygons.each(function (polygon) {
+                    if (polygon.isVisible) {
+                        surface.drawPolygon(polygon.screenVertices, material);
+                    }
+                });
+             */
+
         }
     };
 
