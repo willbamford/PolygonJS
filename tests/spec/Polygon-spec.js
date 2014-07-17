2996ae16e277f11b8eea0ee33cddcea00d4becb0
Will Bamford <w.bamford@gmail.com>
2014-07-17 20:05:31 +0100

    Unit tests for polygon changes.

--- tests/spec/Polygon-spec.js
+++ tests/spec/Polygon-spec.js
@@ -29,6 +29,16 @@ define(
                     expect(polygon.type).toBe('polygon');
                 });
 
+                it('should have a default style of "white"', function () {
+                    var polygon = Polygon.create();
+                    expect(polygon.style).toBe('white');
+                });
+
+                it('should be able to initialise with a style', function () {
+                    var polygon = Polygon.create({style: 'red'});
+                    expect(polygon.style).toBe('red');
+                });
+
                 it('should be able initialise with vertices and normal', function () {
                     var polygon = Polygon.create({
                         vertices: vertices,
@@ -42,13 +52,18 @@ define(
                     expect(polygon.viewVertices).toBe(viewVertices);
                     expect(polygon.screenVertices).toBe(screenVertices);
                     expect(polygon.normal).toBe(normal);
-                    expect(polygon.viewNormal).not.toBe(polygon.normal);
-                    expect(polygon.viewNormal.equals(polygon.normal)).toBeTruthy();
+                    expect(polygon.worldNormal).not.toBe(polygon.normal);
+                    expect(polygon.worldNormal.equals(polygon.normal)).toBeTruthy();
                 });
 
                 it('should not be culled initially', function () {
                     var polygon = Polygon.create();
                     expect(polygon.isCulled).toBeFalsy();
+                });
+
+                it('should have a distance to camera value of zero initially', function () {
+                    var polygon = Polygon.create();
+                    expect(polygon.distanceToCamera).toBe(0);
                 });
             });
         });
